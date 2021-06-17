d4371897d637a6a728320bdc7c4ab36617735198
yubingjiaocn <yubingjiaocn@gmail.com>
2021-06-17 15:41:18 +0800

    Add multi-thread and ffprobe for health check

--- functions/healthchecker/app.py
+++ functions/healthchecker/app.py
@@ -1,16 +1,44 @@
 import os
+import subprocess
 import time
 import boto3
-import json
+import botocore
 import requests
-from boto3.dynamodb.conditions import Key, Attr
+import threading
+from boto3.dynamodb.conditions import Attr
 from requests.adapters import HTTPAdapter
 
 tablename = os.environ.get('TABLE_NAME')
 
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.resource('dynamodb', config=botocore.client.Config(max_pool_connections=50))
 table = dynamodb.Table(tablename)
 
+def check_livestream(url, id):
+    available = 0
+    s = requests.Session()
+    s.mount('http://', HTTPAdapter(max_retries = 3))
+    s.mount('https://', HTTPAdapter(max_retries = 3))
+
+    try:
+        r = s.get(url, timeout=(0.3,1))
+        if r.ok:
+            #print('Livestream ' + id + ' master playlist OK')
+            cmd = 'ffprobe -hide_banner -loglevel error ' + url
+            try:
+                subprocess.run(cmd, shell=True, check=True)
+                available = 1
+                print('Livestream ' + id + ' playlist OK')
+            except subprocess.CalledProcessError: 
+                print('Livestream ' + id + ' has invalid media file')
+        else:
+            print("Livestream " + id + ' master playlist failed (404)')
+    except requests.exceptions.RequestException as e:
+        print("Livestream " + id + ' master playlist failed (Unable to connect)')
+        
+    if available == 0:
+        table.update_item(Key = {'id': id}, 
+                      UpdateExpression = 'SET available = :val1', 
+                      ExpressionAttributeValues = {':val1': available})
 
 def lambda_handler(event, context):
     timestamp = int(time.time())
@@ -18,27 +46,19 @@ def lambda_handler(event, context):
         FilterExpression = Attr('available').eq(1),
     )
     items = resp['Items']
+    print("Checking " + str(len(items)) + " live stream")
+    threads = []
     for item in items:
-        available = 0
-        s = requests.Session()
-        s.mount('http://', HTTPAdapter(max_retries = 3))
-        s.mount('https://', HTTPAdapter(max_retries = 3))
-
+        print("Start checking " + item["id"])
         try:
-            r = s.get(item["url"], timeout=2)
-            if r.ok:
-                print('Check livestream ' + item["id"] + ' OK')
-                # Add check on empty m3u8, find an empty one
-                available = 1
-            else:
-                print("Livestream " + item["id"] + ' encounter 404')
-        except requests.exceptions.RequestException as e:
-            print("Livestream " + item["id"] + ' unable to connect')
-
-        table.update_item(Key = {'id': item["id"]}, 
-                          UpdateExpression = 'SET available = :val1', 
-                          ExpressionAttributeValues = {':val1': available})
-
+            t = threading.Thread(target=check_livestream, args=(item["url"], item["id"]))
+            threads.append(t)
+            t.start()
+        except:
+            print("create thread failed on " + item["id"])
+    for t in threads:
+        t.join()    
+                   
     return {
         "statusCode": 200
     }
